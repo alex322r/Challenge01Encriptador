42a49fec0198dddd530457b6d521a132919cc78c
Alexis <alexisroor@gmail.com>
2022-09-01 19:29:08 -0500

    js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-alert ("hola");
+const inputTxt = document.querySelector(".txt-encriptar");
+const mensaje = document.querySelector(".txt-mensaje");
+const aviso = document.querySelector(".mensaje-aviso");
+const btnCopiar = document.querySelectorAll(".btn-copiar");
+
+function desencriptar(stringEncriptada) {
+
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o","ober"], ["u","ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+    
+    
+}
+
