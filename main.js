554535b4777c24168e94eba4dd09779c34ffc319
Alexis <alexisroor@gmail.com>
2022-09-01 20:27:58 -0500

    js

--- main.js
+++ main.js
@@ -1,13 +1,53 @@
 const inputTxt = document.querySelector(".txt-encriptar");
 const mensaje = document.querySelector(".txt-mensaje");
-const aviso = document.querySelector(".mensaje-aviso");
-const btnCopiar = document.querySelectorAll(".btn-copiar");
+const aviso = document.querySelector("#mensaje-aviso");
+const btnCopiar = document.querySelector("#btn-copiar");
 
-function desencriptar(stringEncriptada) {
 
+function btnEncriptar() {
+    const textoEncriptado = encriptar(inputTxt.value);
+    mensaje.value = textoEncriptado;
+    mensaje.style.background="#292b36";
+    inputTxt.value = "";
+    btnCopiar.style.display = "block";
+    aviso.style.display = "none";
+
+}
+
+function encriptar(stringEncriptada) {
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o","ober"], ["u","ufat"]];
     stringEncriptada = stringEncriptada.toLowerCase();
-    
+
+    for(let i=0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }                
+    }
+    return stringEncriptada;
+
     
 }
+
+
+function btnDesencriptar() {
+    const textoEncriptado = desencriptar(inputTexto.value)
+    mensaje.value = textoEncriptado
+    inputTexto.value = ""  
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [ ["e", "enter"], ["i", "imes"], ["a", "ai"], ["o","ober"], ["u","ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i=0; i < matrizCodigo.length; i++) {
+        if(stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+
+    return stringDesencriptada;
+}
+
+
+//botón copiar
 
